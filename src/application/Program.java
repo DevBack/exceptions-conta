ba86fce9a32471413ba4c3574b67d51ab078ad31
DevBack <heltonlima.ti@live.com>
2023-01-18 22:42:09 -0300

    Initial Implementation

---
+++ src/application/Program.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Informe os Dados da Conta\n");
+		System.out.println("Número:");
+		int number = scanner.nextInt();
+		System.out.println("Titular:");
+		scanner.nextLine();
+		String holder = scanner.nextLine();
+		System.out.println("Saldo Inicial:");
+		double balance = scanner.nextDouble();
+		System.out.println("Limite de Saque:");
+		double withdrawLimit = scanner.nextDouble();
+		
+		Account account = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.println();
+		System.out.println("Informe a Quantia para Sacar: ");
+		double amount = scanner.nextDouble();
+		
+		account.withdraw(amount);
+		System.out.printf("Novo Saldo: %.2f%n", account.getBalance());
+		
+		scanner.close();
+	}
+}
